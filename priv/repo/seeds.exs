eb663379a6a6931e0d161c85fd1dd482161c47db
l-tn3314 <l.tn3314@gmail.com>
2019-04-04 19:57:24 -0400

    updating seeds to location format

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,11 +16,11 @@ alias Project2.Users.User
 
 pwhash = Argon2.hash_pwd_salt("password")
 
-Repo.insert!(%User{email: "alice@example.com", display_name: "alice", password_hash: pwhash, hometown: "San Bernadino", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
-Repo.insert!(%User{email: "bob@example.com", display_name: "bobby", password_hash: pwhash, hometown: "Boston", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
-Repo.insert!(%User{email: "cal@example.com", display_name: "cal", password_hash: pwhash, hometown: "Boston", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
-Repo.insert!(%User{email: "dee@example.com", display_name: "dee", password_hash: pwhash, hometown: "New York", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
-Repo.insert!(%User{email: "eni@example.com", display_name: "eni", password_hash: pwhash, hometown: "Atlanta", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
+Repo.insert!(%User{email: "alice@example.com", display_name: "alice", password_hash: pwhash, hometown: "San Bernadino, US", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
+Repo.insert!(%User{email: "bob@example.com", display_name: "bobby", password_hash: pwhash, hometown: "Boston, US", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
+Repo.insert!(%User{email: "cal@example.com", display_name: "cal", password_hash: pwhash, hometown: "Boston, US", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
+Repo.insert!(%User{email: "dee@example.com", display_name: "dee", password_hash: pwhash, hometown: "New York, US", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
+Repo.insert!(%User{email: "eni@example.com", display_name: "eni", password_hash: pwhash, hometown: "Atlanta, US", pw_last_try: DateTime.truncate(DateTime.utc_now(), :second)})
 
 alias Project2.Friends.Friend
 Repo.insert!(%Friend{lower_user_id: 1, higher_user_id: 2})
